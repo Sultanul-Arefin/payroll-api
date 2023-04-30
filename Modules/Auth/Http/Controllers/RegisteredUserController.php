443ef13458f656f014be514605211f1fec896fc8
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-04-30 13:33:06 +0600

    fix: password confirmation validation

--- Modules/Auth/Http/Controllers/RegisteredUserController.php
+++ Modules/Auth/Http/Controllers/RegisteredUserController.php
@@ -23,7 +23,8 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            // 'password' => ['required', 'confirmed', Rules\Passwor::defaults()],
+            // 'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', 'confirmed'],
         ]);
 
         $user = User::create([
