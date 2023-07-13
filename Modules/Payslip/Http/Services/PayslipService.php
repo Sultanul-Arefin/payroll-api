4e3acd33c9df90541c1d24d85608f710376a548a
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-07-13 18:16:42 +0600

    update: relation added in salaryItemsCategory, salaryItemsName, employeeSalaryItem entities

--- Modules/Payslip/Http/Services/PayslipService.php
+++ Modules/Payslip/Http/Services/PayslipService.php
@@ -2,9 +2,15 @@
 
 namespace Modules\Payslip\Http\Services;
 
+use Modules\EmployeeSalaryItems\Entities\EmployeeSalaryItem;
+
 class PayslipService
 {
     function getCategoryOneData($employee_id) {
-        return $employee_id;
+        $data = EmployeeSalaryItem::query()
+            // ->whereHas('salaryItemsName')
+            // ->where('company_id', auth()->user()->company_id)
+            // ->where('employee_id', $employee_id)
+            ->get();
     }
 }
--- Modules/SalaryItemsCategory/Entities/SalaryItemsCategory.php
+++ Modules/SalaryItemsCategory/Entities/SalaryItemsCategory.php
@@ -4,6 +4,8 @@ namespace Modules\SalaryItemsCategory\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\SalaryItemsName\Entities\SalaryItemsName;
 
 class SalaryItemsCategory extends Model
 {
@@ -14,5 +16,12 @@ class SalaryItemsCategory extends Model
     protected static function newFactory()
     {
         return \Modules\SalaryItemsCategory\Database\factories\SalaryItemsCategoryFactory::new();
+    }
+
+    /**
+     * @return HasMany
+     */
+    function salaryItemsName(): HasMany {
+        return $this->hasMany(SalaryItemsName::class, 'salary_items_category_id', 'id');
     }
 }
