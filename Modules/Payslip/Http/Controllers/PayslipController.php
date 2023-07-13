1c475448f032ddfbb7d51bec7b9cd6d0b7d83fdd
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-07-13 17:06:12 +0600

    update: Payslip Service Created

--- Modules/Payslip/Http/Controllers/PayslipController.php
+++ Modules/Payslip/Http/Controllers/PayslipController.php
@@ -5,11 +5,29 @@ namespace Modules\Payslip\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Payslip\Http\Services\PayslipService;
 
 class PayslipController extends Controller
 {
+    function __construct(
+        public PayslipService $payslipService
+    ) {
+        
+    }
     public function salary_information_before_running_payslip(Request $request)
     {
-        return $request->all();
+        $request->validate([
+            'employee_id' => 'required'
+        ]);
+        $category_1 = $this->payslipService->getCategoryOneData($request->employee_id);
+        return $category_1;
+    }
+
+    function run_payslip() {
+        
+    }
+
+    function preview_payslip() {
+        
     }
 }
---
+++ Modules/Payslip/Http/Services/PayslipService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Payslip\Http\Services;
+
+class PayslipService
+{
+    function getCategoryOneData($employee_id) {
+        return $employee_id;
+    }
+}
