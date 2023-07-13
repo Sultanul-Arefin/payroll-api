a24f5a39bbb9fcc80c425385e2234e3e6735ef73
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-07-13 18:31:24 +0600

    update: checked employee salary items for category 1

--- Modules/Payslip/Http/Services/PayslipService.php
+++ Modules/Payslip/Http/Services/PayslipService.php
@@ -2,15 +2,21 @@
 
 namespace Modules\Payslip\Http\Services;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Modules\EmployeeSalaryItems\Entities\EmployeeSalaryItem;
 
 class PayslipService
 {
     function getCategoryOneData($employee_id) {
         $data = EmployeeSalaryItem::query()
-            // ->whereHas('salaryItemsName')
-            // ->where('company_id', auth()->user()->company_id)
-            // ->where('employee_id', $employee_id)
+            ->where('company_id', auth()->user()->company_id)
+            ->where('employee_id', $employee_id)
+            ->whereHas(
+                'salaryItemsName', function(Builder $query){
+                    $query->where('salary_items_category_id', 1);
+                }
+            )
             ->get();
+        return $data;
     }
 }
