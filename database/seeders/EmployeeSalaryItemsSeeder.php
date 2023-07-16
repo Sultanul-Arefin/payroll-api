0d3dfb94fb824596edc71b204dbdea4bfcd26b45
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-07-16 17:18:20 +0600

    update: salary items associated with employee added

---
+++ database/seeders/EmployeeSalaryItemsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Modules\EmployeeSalaryItems\Entities\EmployeeSalaryItem;
+use Modules\SalaryItemsName\Entities\SalaryItemsName;
+
+class EmployeeSalaryItemsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $salary_items_names = SalaryItemsName::where('company_id', 1)->get();
+        foreach($salary_items_names as $value){
+            EmployeeSalaryItem::create([
+                'salary_item_id' => $value->id,
+                'employee_id' => 1,
+                'company_id' => 1,
+                'is_percentage' => 0,
+                'is_general' => 0,
+                'amount' => 100
+            ]);
+        }
+    }
+}
