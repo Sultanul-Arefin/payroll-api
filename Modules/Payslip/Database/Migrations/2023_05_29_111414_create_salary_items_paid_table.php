f21131b9389c5679b7f5ac97d092100a27c047b6
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-07-25 15:50:54 +0600

    update: salary items paid table updated in payslip section

--- Modules/Payslip/Database/Migrations/2023_05_29_111414_create_salary_items_paid_table.php
+++ Modules/Payslip/Database/Migrations/2023_05_29_111414_create_salary_items_paid_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('salary_items_paid', function (Blueprint $table) {
             $table->id();
-
+            $table
+                ->foreignId('payslip_id')
+                ->constrained('payslips', 'id')
+                ->cascadeOnDelete();
+            $table
+                ->foreignId('salary_item_id')
+                ->constrained('salary_items_names', 'id')
+                ->cascadeOnDelete();
+            $table->double('amount', 8, 2); // can store total 8 digits, 6 before decimal point & 2 after decimal point
             $table->timestamps();
         });
     }
