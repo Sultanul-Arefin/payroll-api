b0cf2ec44505b83d0f48ddcde3ec27c6ae0d2364
akazad1235 <akazad914@gmail.com>
2023-07-26 13:45:12 +0600

    leave status [approve and denied] done

--- Modules/LeaveManagement/Http/Controllers/LeaveManagementController.php
+++ Modules/LeaveManagement/Http/Controllers/LeaveManagementController.php
@@ -2,9 +2,12 @@
 
 namespace Modules\LeaveManagement\Http\Controllers;
 
+use App\Exceptions\CustomException;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Mockery\Exception;
+use Modules\LeaveManagement\Entities\UserLeave;
 use Modules\LeaveManagement\Http\Requests\LeaveStoreRequest;
 use Modules\LeaveManagement\Http\Resources\LeaveListResource;
 use Modules\LeaveManagement\Http\Resources\LeaveResource;
@@ -39,6 +42,29 @@ class LeaveManagementController extends Controller
        $LeaveList =  $this->leaveRepository->leave_list();
        $LeavesCollection =  LeaveListResource::collection($LeaveList);
         return apiResponse($LeavesCollection, 'Successfully Get Leave List', 'success', '200');
+    }
+    public function leave_status(Request $request){
+        $request->validate([
+            'user_leave_id' => 'required',
+            'status' => 'required|in:1,3',
+            'action_message' => 'required'
+        ]);
+        try {
+            $userLeave = UserLeave::findOrFail($request->user_leave_id);
+            $update =  $userLeave->update([
+                'status' => $request->status,
+                'action_message' => $request->action_message,
+                'action_by' => auth()->user()->id
+            ]);
+            if($update){
+                return apiResponse(null, 'Successfully Leave Updated', 'success', '200');
+            }else{
+                throw new \Exception('');
+            }
+
+        }catch (\Exception $ex){
+            throw new CustomException('oops! something wrong, please try again', 404);
+        }
     }
 
 }
--- Modules/LeaveManagement/Routes/api.php
+++ Modules/LeaveManagement/Routes/api.php
@@ -8,6 +8,7 @@ Route::middleware(['json.response'])->prefix('v1')->group(function(){
         Route::get('leave-types', [LeaveManagementController::class, 'leave_types']);
         Route::post('leave-store', [LeaveManagementController::class, 'leave_store']);
         Route::get('leave-list', [LeaveManagementController::class, 'leave_list']);
+        Route::post('leave-status', [LeaveManagementController::class, 'leave_status']);
 
     });
 });
