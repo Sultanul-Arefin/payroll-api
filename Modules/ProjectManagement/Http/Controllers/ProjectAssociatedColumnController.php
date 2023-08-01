029d0d1378c4554253054a0ae081b0ce630edf70
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-08-01 13:24:24 +0600

    update: store column updated for column_position key, & change column index function completed

--- Modules/ProjectManagement/Http/Controllers/ProjectAssociatedColumnController.php
+++ Modules/ProjectManagement/Http/Controllers/ProjectAssociatedColumnController.php
@@ -34,16 +34,45 @@ class ProjectAssociatedColumnController extends Controller
      */
     public function store(Request $request)
     {
+        $column_value = ProjectAssociatedColumn::where('project_id', $request->project_id)->get();
+        $max_column_value = $column_value->max('column_position') ?? 0;
+
         $column = ProjectAssociatedColumn::create([
             'project_id' => $request->project_id,
             'project_column_name' => $request->project_column_name,
-            'created_by' => auth()->user()->id
+            'created_by' => auth()->user()->id,
+            'column_position' => ++$max_column_value
         ]);
 
         return apiResponse(
             data: $column,
             message: "Project Column Created Successfully",
             status: 'success'
+        );
+    }
+
+    function change_column_index(Request $request) {
+        $request->validate([
+            'current_column_id' => 'required|integer',
+            'target_column_id' => 'required|integer'
+        ]);
+        $current_column = ProjectAssociatedColumn::where('id', $request->current_column_id)->first();
+        $target_column = ProjectAssociatedColumn::where('id', $request->target_column_id)->first();
+
+        $current_column_value = $current_column->column_position;
+
+        // update
+        $current_column->update([
+            'column_position' => $target_column->column_position
+        ]);
+        $target_column->update([
+            'column_position' => $current_column_value
+        ]);
+
+        return apiResponse(
+            data: null,
+            message: "Column Position Updated Successfully",
+            status: "success"
         );
     }
 }
