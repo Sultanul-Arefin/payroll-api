b45e7c70838bcfe902345d10ef033f96f3a6b70f
Sultanul-Arfin <fahimsultan4@gmail.com>
2023-08-12 23:49:29 +0600

    update: api added

--- Modules/SalaryItemsCategory/Routes/api.php
+++ Modules/SalaryItemsCategory/Routes/api.php
@@ -18,5 +18,7 @@ Route::middleware(['json.response'])->prefix('v1')->group(function () {
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('salary-items-categories', [SalaryItemsCategoryController::class, 'index'])
             ->name('salary-items-category');
+        Route::get('salary-items-value-against-category', [SalaryItemsCategoryController::class, 'getSalaryItemsValue'])
+            ->name('salary-items-value-against-category');
     });
 });
