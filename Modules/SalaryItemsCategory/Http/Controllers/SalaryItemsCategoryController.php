bc07ac709aab613357627a96857f92407f6057a7
Sultanul-Arfin <fahimsultan4@gmail.com>
2023-08-13 01:24:41 +0600

    update: details wages added against category

--- Modules/SalaryItemsCategory/Http/Controllers/SalaryItemsCategoryController.php
+++ Modules/SalaryItemsCategory/Http/Controllers/SalaryItemsCategoryController.php
@@ -5,13 +5,16 @@ namespace Modules\SalaryItemsCategory\Http\Controllers;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\SalaryItemsCategory\Http\Resources\DetailsWagesAgainstCategoryResource;
 use Modules\SalaryItemsCategory\Http\Resources\SalaryItemsCategoryResource;
+use Modules\SalaryItemsCategory\Http\Services\SalaryItemsCategoryService;
 use Modules\SalaryItemsCategory\Repositories\Interfaces\SalaryItemsCategoryInterface;
 
 class SalaryItemsCategoryController extends Controller
 {
     public function __construct(
-        private SalaryItemsCategoryInterface $salaryItemsCategoryRepo
+        private SalaryItemsCategoryInterface $salaryItemsCategoryRepo,
+        private SalaryItemsCategoryService $salaryItemsCategoryService
     ){
     }
 
@@ -36,5 +39,14 @@ class SalaryItemsCategoryController extends Controller
         $request->validate([
             'salary_items_category_id' => 'required|exists:salary_items_categories,id'
         ]);
+        $rows = 15;
+        if (request()?->has('rows')) {
+            $rows = (int) request('rows');
+        }
+        return DetailsWagesAgainstCategoryResource::collection(
+            $this->salaryItemsCategoryService->get_salary_items_name_with_employee(
+                $request->salary_items_category_id, $rows
+            )
+        );
     }
 }
---
+++ Modules/SalaryItemsCategory/Http/Resources/DetailsWagesAgainstCategoryResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\SalaryItemsCategory\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use JsonSerializable;
+
+class DetailsWagesAgainstCategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            $this->merge(
+                Arr::only(parent::toArray($request), [
+                    'id',
+                ])
+            ),
+            'item_name' => $this->salaryItemsName?->name,
+            'category' => $this->salaryItemsName?->salaryItemsCategory?->name,
+            'salary_amount' => $this->amount,
+            'issue_to' => $this->employee?->name,
+            'is_general' => 0
+        ];
+    }
+}
