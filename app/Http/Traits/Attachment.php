53fe6d6bcbf9b022aed3a3f0ec6f80d31bcbd3a0
akazad1235 <akazad914@gmail.com>
2023-08-14 16:55:55 +0600

    employee multiple attachment done

--- app/Http/Traits/Attachment.php
+++ app/Http/Traits/Attachment.php
@@ -24,6 +24,30 @@ trait Attachment{
             throw new CustomException('Something Wrong!');
             return false;
         }
+    }
+    public function uploadMultipleAttachment($files, string $storagePath=''){
+        $finalFilePath = [];
+        if($files){
+            foreach($files as $file){
+                $fileName = rand(0, 999999999) . '_' . date('Ymdhis').'_' . rand(100, 999999999) . '.' . $file->getClientOriginalExtension();
+                $file->storeAs($storagePath, $fileName, 'public');
+                $name = "{$storagePath}/{$fileName}";
+                array_push($finalFilePath, $name);
+            }
+        }
+        return ['fileName' => $finalFilePath, 'message' => 'somethings..'];
+    }
+
+    public function deleteMultipleAttachment($fileNames){
+        foreach($fileNames as $fileName){
+            $existFile =  Storage::disk('public')->exists($fileName);
+            if (!empty($existFile)){
+                Storage::disk('public')->delete($fileName);
+            }else{
+                throw new CustomException('Something Wrong!');
+                 break;
+            }
+        }
 
     }
 
