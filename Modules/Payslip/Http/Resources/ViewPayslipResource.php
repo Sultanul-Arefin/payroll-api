a3ebcd82973e79cabfa85240ff165bd04739a625
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-10-01 01:21:50 +0600

    update: updated payslip resource

---
+++ Modules/Payslip/Http/Resources/ViewPayslipResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\Payslip\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use JsonSerializable;
+
+class ViewPayslipResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'items_details' => $this->payslip_details,
+            'payment_date' => date('Y-m-d H:i:s'),
+            'taxable_allowance' => $this->taxable_allowance,
+            'non_taxable_allowance' => $this->non_taxable_allowance,
+            'total_gross_pay' => $this->gross_pay_before_tax,
+            'taxable_gross_pay' => $this->gross_pay_before_tax,
+            'tax_amount' => $this->tax_value + $this->post_tax_value,
+            'pay_due_before_deduction' => $this->pay_deduction,
+            'staff_social_charges' => 0.00,
+            'total_net_pay' => $this->net_pay
+        ];
+    }
+}
