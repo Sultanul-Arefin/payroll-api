0239aca7d3ca7d1b1733c980eac989110bfd438b
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-10-07 05:45:44 +0600

    logic updated for company & employee deductions

--- Modules/EmployeeSalaryItems/Http/Controllers/EmployeeSalaryItemsController.php
+++ Modules/EmployeeSalaryItems/Http/Controllers/EmployeeSalaryItemsController.php
@@ -6,14 +6,26 @@ use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
+use Modules\EmployeeSalaryItems\Entities\DeductionDetails;
 use Modules\EmployeeSalaryItems\Entities\EmployeeSalaryItem;
 use Modules\EmployeeSalaryItems\Http\Requests\StoreEmployeeSalaryItems;
+use Modules\SalaryItemsName\Entities\SalaryItemsName;
 
 class EmployeeSalaryItemsController extends Controller
 {
     public function store(StoreEmployeeSalaryItems $request)
     {
-        $employee_salary = DB::transaction(function() use($request){
+        // validation for category_id 7 & 8
+        $check_category = SalaryItemsName::where('id', $request->salary_item_id)->first();
+        if($check_category->salary_items_category_id == 7 || $check_category->salary_items_category_id == 8){
+            $request->validate([
+                'company_deduction' => 'required', // '1|2'
+                'employee_contribution' => $check_category->salary_items_category_id == 7 ? 'required' : 'nullable',
+                'employee_deduction' => $check_category->salary_items_category_id == 8 ? 'required' : 'nullable',// 1|3
+                'amount' => 'nullable'
+            ]);
+        }
+        $employee_salary = DB::transaction(function() use($request, $check_category){
             $employee_salary = EmployeeSalaryItem::create([
                 'salary_item_id' => $request->salary_item_id,
                 'company_id' => auth()->user()->company_id,
@@ -22,6 +34,13 @@ class EmployeeSalaryItemsController extends Controller
                 'employee_id' => $request->employee_id ?? null,
                 'amount' => $request->amount ?? null
             ]);
+            if($check_category->salary_items_category_id == 7 || $check_category->salary_items_category_id == 8){
+                $create_deduction = DeductionDetails::create([
+                    'employee_salary_item_id' => $employee_salary->id,
+                    'employee_amount' => $check_category->salary_items_category_id == 7 ? $request->employee_contribution : $request->employee_deduction,
+                    'government_or_company_amount' => $request->company_deduction
+                ]);
+            }
             return $employee_salary;
         });
         return apiResponse(
