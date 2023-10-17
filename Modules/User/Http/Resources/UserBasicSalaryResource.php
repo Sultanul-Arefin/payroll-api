297acb406588eb670718f27e4e46d9b07bb21d5c
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-10-17 15:55:01 +0600

    added a new resource for getting the salary items

---
+++ Modules/User/Http/Resources/UserBasicSalaryResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\User\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use JsonSerializable;
+
+class UserBasicSalaryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            $this->merge(
+                Arr::only(parent::toArray($request), [
+                    'id',
+                    'name'
+                ])
+            ),
+            'amount' => $this->employeeSalaryItem->where('employee_id', auth()->user()->id)->first()?->amount
+        ];
+    }
+}
