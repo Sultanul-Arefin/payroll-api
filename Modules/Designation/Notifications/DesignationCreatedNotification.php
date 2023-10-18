954ac51e346f1e53bbf453b35ba43bdd7856f71a
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-10-19 05:18:18 +0600

    designation created notification file created

---
+++ Modules/Designation/Notifications/DesignationCreatedNotification.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Modules\Designation\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class DesignationCreatedNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(public $created_by, public $designation)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    // public function toMail($notifiable)
+    // {
+    //     return (new MailMessage)
+    //                 ->line('The introduction to the notification.')
+    //                 ->action('Notification Action', 'https://laravel.com')
+    //                 ->line('Thank you for using our application!');
+    // }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'title'         => 'Designation Creation',
+            'description'   => 'A Designation named ' . $this->designation->name . ' Created!',
+            'action_by'     => [
+                'name'  => $this->created_by->name,
+                'email' => $this->created_by->email,
+                'phone' => $this->created_by->phone
+            ],
+            'action_at'     => date('Y-m-d H:i:s'),
+            'type'          => 'Company',
+            'color'         => ''
+        ];
+    }
+}
