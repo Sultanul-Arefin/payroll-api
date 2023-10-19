0d2b96e6e1a86c66b3a9c4249b08cafc1a355a3b
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-10-20 04:35:42 +0600

    total staff cost & total paid salary endpoint created

--- Modules/Dashboard/Routes/api.php
+++ Modules/Dashboard/Routes/api.php
@@ -20,6 +20,8 @@ Route::middleware(['json.response'])->prefix('v1')->group(function () {
             Route::get('/total/employee', [DashboardController::class, 'totalEmployee']);
             Route::get('/total/department', [DashboardController::class, 'totalDepartment']);
             Route::get('/holiday', [DashboardController::class, 'holiday']);
+            Route::get('total-paid-salary', [DashboardController::class, 'total_paid_salary']);
+            Route::get('total-staff-cost', [DashboardController::class, 'total_staff_cost']);
         });
 
 
