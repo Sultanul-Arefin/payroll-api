4bf091cb790f2bf43499b03eb6be9bb341e7858f
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-11-07 12:53:46 +0600

    associated package relation added

--- Modules/Package/Entities/Package.php
+++ Modules/Package/Entities/Package.php
@@ -4,6 +4,7 @@ namespace Modules\Package\Entities;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Modules\Package\Database\factories\PackageFactory;
 
 class Package extends Model
@@ -19,5 +20,12 @@ class Package extends Model
     protected static function newFactory()
     {
         return PackageFactory::new();
+    }
+
+    /**
+     * @return HasOne
+     */
+    function associated_package(): HasOne {
+        return $this->hasOne(CompanyAssociatedWithPackage::class, 'package_id', 'id');
     }
 }
