766ac9fd2e7f73004f537f2814c4f3df5511dbfc
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-11-19 10:58:56 +0600

    country id when registering an user

--- Modules/Auth/Http/Controllers/RegisteredUserController.php
+++ Modules/Auth/Http/Controllers/RegisteredUserController.php
@@ -56,6 +56,7 @@ class RegisteredUserController extends Controller
                 'user_area' => $request->user_area,
                 'user_city' => $request->user_city,
                 'user_phone' => $request->user_phone,
+                'country_id' => $request->country_id
                 // 'user_image' => $request->user_address ?? null,
             ]);
             // $user->assignRole('super-admin');
