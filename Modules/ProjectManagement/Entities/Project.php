3d8cb74d39017150eceee12dd33c113e2563099f
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-12-11 00:34:00 +0600

    project model updated

--- Modules/ProjectManagement/Entities/Project.php
+++ Modules/ProjectManagement/Entities/Project.php
@@ -5,10 +5,11 @@ namespace Modules\ProjectManagement\Entities;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Notifications\Notifiable;
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'project_title',
