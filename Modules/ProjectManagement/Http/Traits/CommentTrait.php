1aeb50459f6d247c746f8ea488f8f4376a74f35d
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-12-11 00:35:01 +0600

    comment trait updated

--- Modules/ProjectManagement/Http/Traits/CommentTrait.php
+++ Modules/ProjectManagement/Http/Traits/CommentTrait.php
@@ -5,6 +5,7 @@ namespace Modules\ProjectManagement\Http\Traits;
 use Modules\ProjectManagement\Entities\Task;
 use Modules\ProjectManagement\Entities\TaskComment;
 use Modules\ProjectManagement\Http\Requests\StoreCommentRequest;
+use Modules\ProjectManagement\Notifications\ProjectManagementNotification;
 
 trait CommentTrait
 {
@@ -14,6 +15,21 @@ trait CommentTrait
             'comments' => $request->comments,
             'comment_by' => auth()->user()->id
         ]);
+        // TASK COMMENT CREATION NOTIFICATION
+        $project = $task->project_associated_column->project;
+        $data = [
+            'title' => 'Comment Created',
+            'description' => 'A New Comment Has Been Created in Task '. $task->task_title,
+            'action' => [
+                'name' => auth()->user()->name,
+                'email' => auth()->user()->email,
+                'phone' => auth()->user()->phone,
+            ],
+            'action_at' => date('Y-m-d H:i:s'),
+            'type' => 'Project Management',
+            'color' => '',
+        ];
+        $project->notify(new ProjectManagementNotification($data));
         return apiResponse(
             data: $store,
             message: 'Comment Added Successfully'
