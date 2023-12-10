7d7b04c58af5466b8e15bf97ca653fd2e30e5b13
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-12-10 09:06:04 +0600

    task details response updated

--- Modules/ProjectManagement/Http/Traits/TasksTrait.php
+++ Modules/ProjectManagement/Http/Traits/TasksTrait.php
@@ -20,6 +20,21 @@ trait TasksTrait
             }
             $task[$key]['users'] = $asso_users;
             unset($task[$key]['associated_users']);
+
+            // comments
+            $comments = $value->comments;
+            $asso_comments = [];
+            if (! is_null($comments)) {
+                foreach ($comments as $comment) {
+                    array_push($asso_comments, [
+                        'id' => $comment->id,
+                        'comment' => $comment->comments,
+                        'comment_by' => $comment->user->name,
+                    ]);
+                }
+            }
+            $task[$key]['task_comment'] = $asso_comments;
+            unset($task[$key]['comments']);
         }
 
         return $task;
