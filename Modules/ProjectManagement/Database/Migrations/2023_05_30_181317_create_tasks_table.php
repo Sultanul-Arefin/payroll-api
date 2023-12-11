11dfc0f65cf47e2909b9cf0580dc41b0ad846a9a
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-12-11 11:20:20 +0600

    status added in tasks table

--- Modules/ProjectManagement/Database/Migrations/2023_05_30_181317_create_tasks_table.php
+++ Modules/ProjectManagement/Database/Migrations/2023_05_30_181317_create_tasks_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Modules\ProjectManagement\Entities\Task;
 
 return new class extends Migration
 {
@@ -28,6 +29,7 @@ return new class extends Migration
             $table->string('estimation_hour')->nullable();
             $table->date('start_date_time')->nullable();
             $table->date('end_date_time')->nullable();
+            $table->integer('status')->default(Task::BACKLOG);
             $table
                 ->foreignId('created_by')
                 ->constrained('users', 'id')
