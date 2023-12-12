db789d131517b6a26d350a162e0a783d3a6cbfb0
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-12-12 11:26:46 +0600

    new resource for task overview

---
+++ Modules/ProjectManagement/Http/Resources/TaskOverviewResource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Modules\ProjectManagement\Http\Resources;
+
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Arr;
+use JsonSerializable;
+use Modules\ProjectManagement\Entities\Task;
+
+class TaskOverviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            $this->merge(
+                Arr::only(parent::toArray($request), [
+                    'id',
+                ])
+            ),
+            'title' => $this->task_title,
+            'estimated_hour' => $this->estimation_hour,
+            'start_date' => $this->start_date_time,
+            'end_date' => $this->end_date_time,
+            'status' => $this->status,
+            'color' => $this->getColor($this->status),
+            'percentage' => $this->getPercentage()
+        ];
+    }
+
+    function getPercentage() {
+        return 0;
+    }
+
+    function getColor($status) {
+        return match ($status) {
+            Task::BACKLOG => 'navy',
+            Task::COMPLETED => 'green',
+            Task::IN_PROGRESS => 'orange',
+            Task::CANCELLED => 'red',
+            default => ''
+        };
+    }
+}
