778c795da41bc6f1816ebbc062f94d95a4c8781a
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-12-20 10:24:27 +0600

    updating time management report logic

--- Modules/TimeManagement/Http/Controllers/TimeManagementController.php
+++ Modules/TimeManagement/Http/Controllers/TimeManagementController.php
@@ -15,7 +15,7 @@ class TimeManagementController extends Controller
 {
     function time_management_report() 
     {
-        $users = User::where('company_id', auth()->user()->id)->get();
+        $users = User::where('company_id', auth()->user()->id)->where('status', User::USER_ACTIVE)->get();
         $response = TimeManagementResource::collection(
             $users
         );
