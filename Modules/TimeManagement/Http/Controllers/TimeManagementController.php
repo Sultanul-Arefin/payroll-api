b88c75a4067598269590c4e50188a14cef68ade6
Sultanul-Arefin <fahimsultan4@gmail.com>
2023-12-21 11:55:25 +0600

    attendance calendar overview per person updated

--- Modules/TimeManagement/Http/Controllers/TimeManagementController.php
+++ Modules/TimeManagement/Http/Controllers/TimeManagementController.php
@@ -13,8 +13,12 @@ use Modules\TimeManagement\Http\Resources\TimeManagementResource;
 
 class TimeManagementController extends Controller
 {
-    function time_management_report() 
+    function time_management_report(Request $request) 
     {
+        $request->validate([
+            'department_id' => 'required|integer',
+            'year' => 'required'
+        ]);
         $users = User::where('company_id', auth()->user()->id)->where('status', User::USER_ACTIVE)->get();
         $response = TimeManagementResource::collection(
             $users
@@ -24,8 +28,12 @@ class TimeManagementController extends Controller
         );
     }
 
-    function attendance_report() 
+    function attendance_report(Request $request) 
     {
+        $request->validate([
+            'from_date' => 'required|date|date_format:Y-m-d',
+            'to_date' => 'required|date|date_format:Y-m-d'
+        ]);
         $users = User::where('company_id', auth()->user()->id)->where('status', User::USER_ACTIVE)->get();
         $response = AttendanceReportResource::collection(
             $users
@@ -45,6 +53,7 @@ class TimeManagementController extends Controller
         ]);
         $attendance = Attendance::query()
                     ->where('user_id', $request->user_id)
+                    ->where('status', Attendance::PRESENT)
                     ->whereBetween(
                         'dates',
                         [
