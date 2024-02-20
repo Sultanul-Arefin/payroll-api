a774fc1bd796dbbdcd8cf75d2e27a1b70a4dfc6d
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-02-20 13:35:50 +0600

    enums added in model

--- app/Models/Support.php
+++ app/Models/Support.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Support extends Model
 {
     use HasFactory;
+
+    public const CLOSED = 0;
+    public const ACTIVE = 1;
+    public const PENDING = 2;
 }
