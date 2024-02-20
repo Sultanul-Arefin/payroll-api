b1efd6adee61d68ff69bd656d5f8504ed4007837
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-02-20 13:36:06 +0600

    migration file updated

--- database/migrations/2024_02_20_072423_create_help_article_categories_table.php
+++ database/migrations/2024_02_20_072423_create_help_article_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('help_article_categories', function (Blueprint $table) {
             $table->id();
+            $table->string('category_name');
             $table->timestamps();
         });
     }
