e4c3fa5f1da8b9dd4c0c60c493afb2f527c90025
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-02-29 13:48:17 +0600

    updated salaryItemsSeeder file

--- database/seeders/SalaryItemsNamesSeeder.php
+++ database/seeders/SalaryItemsNamesSeeder.php
@@ -20,7 +20,7 @@ class SalaryItemsNamesSeeder extends Seeder
                 'Paid Sick Leave Rate',
                 'Unpaid Sick Leave Rate',
                 'Holiday Rate',
-                'Absent',
+                'Absent Rate',
                 'Bonus',
                 'Overtime Rate',
                 'Double Overtime Rate',
