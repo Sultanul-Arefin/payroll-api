a3aaeaeaab2938d294067382de10f69c4f4d1c5f
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-03-04 23:04:08 +0600

    update-password from staff interaction done

--- Modules/User/Http/Traits/UserTrait.php
+++ Modules/User/Http/Traits/UserTrait.php
@@ -44,6 +44,41 @@ trait UserTrait
         );
     }
 
+    public function update_password(User $user, Request $request)
+    {
+        $request->validate([
+            'password' => 'required|string|max:30',
+        ]);
+        $updated_user = $user->update([
+            'password' => Hash::make($request->password)
+        ]);
+
+        return apiResponse(
+            data: null,
+            message: 'Password Successfully Updated'
+        );
+    }
+
+    public function update_role(User $user, Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|exists:users,id',
+            'role_id' => 'required|integer|in:1,2,3',
+            'password' => 'required|string|max:30',
+        ]);
+        $update_user = User::where('id', $request->user_id)->update([
+            'role_id' => $request->role_id,
+            'status' => User::USER_ACTIVE,
+            'password' => Hash::make($request->password),
+            'staff_interaction_panel_status' => User::STAFF_INTERACTION_PANEL_GIVEN
+        ]);
+
+        return apiResponse(
+            data: null,
+            message: 'User Successfully Updated'
+        );
+    }
+
     public function user_information()
     {
         if (auth()->user()->company->associated_package->package->id == '1' || auth()->user()->company->associated_package->package->id == '2' || auth()->user()->company->associated_package->package->id == '3' || auth()->user()->company->associated_package->package->id == '15' || auth()->user()->company->associated_package->package->id == '16' || auth()->user()->company->associated_package->package->id == '17' || auth()->user()->company->associated_package->package->id == '18' || auth()->user()->company->associated_package->package->id == '19' || auth()->user()->company->associated_package->package->id == '20' || auth()->user()->company->associated_package->package->id == '21' || auth()->user()->company->associated_package->package->id == '22') {
