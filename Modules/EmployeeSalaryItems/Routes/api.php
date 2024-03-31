efa9488feb3b43b8a31f5530a232a174f51a6db8
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-04-01 02:54:42 +0600

    salary item amount update & delete endpoint added

--- Modules/EmployeeSalaryItems/Routes/api.php
+++ Modules/EmployeeSalaryItems/Routes/api.php
@@ -8,5 +8,7 @@ Route::middleware(['json.response'])->prefix('v1')->group(function () {
             ->name('salary-items-associate-with-employees');
         Route::get('salary-items-value-against-category', [EmployeeSalaryItemsController::class, 'getSalaryItemsValue'])
             ->name('salary-items-value-against-category');
+        Route::patch('salary-item-amount/{employee_salary_item}', [EmployeeSalaryItemsController::class, 'update_salary_item']);
+        Route::delete('salary-item-amount/{employee_salary_item}', [EmployeeSalaryItemsController::class, 'delete_salary_item']);
     });
 });
