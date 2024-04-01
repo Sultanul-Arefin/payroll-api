807bdcec1408c205d77e3bd6faa383ff24faae6c
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-04-01 13:58:29 +0600

    added internal-report, & department-wise-report endpoint added

--- Modules/Report/routes/api.php
+++ Modules/Report/routes/api.php
@@ -3,17 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Report\Http\Controllers\ReportController;
 
-/*
- *--------------------------------------------------------------------------
- * API Routes
- *--------------------------------------------------------------------------
- *
- * Here is where you can register API routes for your application. These
- * routes are loaded by the RouteServiceProvider within a group which
- * is assigned the "api" middleware group. Enjoy building your API!
- *
-*/
-
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('report', ReportController::class)->names('report');
+Route::middleware(['json.response'])->prefix('v1')->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('department-wise-report', [ReportController::class, 'department_wise_report'])
+            ->name('department-wise-report');
+        Route::get('internal-report', [ReportController::class, 'get_internal_report'])
+            ->name('internal-report');
+    });
 });
