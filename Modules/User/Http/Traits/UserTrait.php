45944ff74e5291a4246d1cc4212fccd8d51864ab
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-04-18 16:10:50 +0600

    changed the update password from admin

--- Modules/User/Http/Traits/UserTrait.php
+++ Modules/User/Http/Traits/UserTrait.php
@@ -50,12 +50,15 @@ trait UserTrait
             'password' => 'required|string|max:30',
             '_method' => 'required'
         ]);
-        $user->update([
-            'password' => Hash::make($request->password)
+        // $user->update([
+        //     'password' => Hash::make($request->password)
+        // ]);
+        User::where('id', $user->id)->update([
+            'password' => bcrypt($request->password)
         ]);
 
         return apiResponse(
-            data: $request->all(),
+            data: null,
             message: 'User Password Successfully Updated'
         );
     }
