12be7101a118e8d03f452b0b66acec8be445fd86
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-05-07 17:27:17 +0600

    admin attendance view

--- Modules/Attendance/Http/Resources/AttendanceResourceForAdmin.php
+++ Modules/Attendance/Http/Resources/AttendanceResourceForAdmin.php
@@ -22,7 +22,7 @@ class AttendanceResourceForAdmin extends JsonResource
             'date' => $this->dates,
             'count' => $this->count_appearances($this->dates),
             'details' => $this->get_info($this->dates),
-            'created_at' => $this->created_at
+            'created_at' => $this->created_at?->format('d-m-Y H:i:s')
         ];
     }
 
