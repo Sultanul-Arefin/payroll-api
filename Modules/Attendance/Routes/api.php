41768c91c51d5befb4e1286407ca7a7edd0aecfd
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-05-13 16:49:04 +0600

    manual attendance by admin api

--- Modules/Attendance/Routes/api.php
+++ Modules/Attendance/Routes/api.php
@@ -6,6 +6,7 @@ use Modules\Attendance\Http\Controllers\LeaveController;
 Route::middleware(['json.response'])->prefix('v1')->group(function () {
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::apiResource('attendance', AttendanceController::class);
+        Route::post('manual-attendance-by-admin', [AttendanceController::class, 'manual_attendance_by_admin']);
         Route::get('requested-attendance', [AttendanceController::class, 'requested_attendance']);
         Route::post('approve-all-attendance-by-date', [AttendanceController::class, 'approve_all_attendance_by_date']);
         Route::post('approve-individual-attendance/{attendance}', [AttendanceController::class, 'approve_individual_attendance']);
