d18b64ae678dea299d21989ed02e27aa2dc0f9a7
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-05-14 14:12:58 +0600

    employee profile updated salary items issue

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -543,7 +543,7 @@ class UserController extends Controller
             }
 
             // update salary items
-            if(isset($request->ordinary_time_rate) && ($request->ordinary_time_rate !== null || $request->ordinary_time_rate !== "null")){
+            if(isset($request->ordinary_time_rate) && !is_null($request->ordinary_time_rate)){
                 $this->update_salary_items($request->all(), $user->id);
             }
 
