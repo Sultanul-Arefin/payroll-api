be90aeb9e8af8bb2194b8f1f4f630b5a03efd31f
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-05-25 23:51:57 +0600

    requested leave list updated

--- Modules/LeaveManagement/Repositories/Classes/LeaveRepository.php
+++ Modules/LeaveManagement/Repositories/Classes/LeaveRepository.php
@@ -70,9 +70,9 @@ class LeaveRepository extends BaseRepository implements LeaveRepositoryInterface
     {
         return UserLeave::query()
             ->when(
-                ! is_null(request('employee_id')),
+                ! is_null(request('user_id')),
                 fn (Builder $builder) => $builder->where(function ($query) {
-                    $query->where('user_id', request('employee_id'));
+                    $query->where('user_id', request('user_id'));
                 })
             )
             ->with('user', 'salary_item', 'leave_details')->get();
