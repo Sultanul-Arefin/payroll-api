ce75bf2cf6294c03156b0705b136b0ce53526414
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-06-02 11:20:43 +0600

    unnecessary line commented

--- Modules/LeaveManagement/Repositories/Classes/LeaveRepository.php
+++ Modules/LeaveManagement/Repositories/Classes/LeaveRepository.php
@@ -70,7 +70,7 @@ class LeaveRepository extends BaseRepository implements LeaveRepositoryInterface
     {
         return UserLeave::query()
             ->when(
-                ! is_null(request('employee_id')),
+                !is_null(request('employee_id')),
                 fn (Builder $builder) => $builder->where(function ($query) {
                     $query->where('employee_id', request('employee_id'));
                 })
