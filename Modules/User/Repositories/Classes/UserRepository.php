a1318f8d4b3480166173e8292d82e6620446650a
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-06-04 13:27:12 +0600

    admin access

--- Modules/User/Repositories/Classes/UserRepository.php
+++ Modules/User/Repositories/Classes/UserRepository.php
@@ -114,9 +114,9 @@ class UserRepository extends BaseRepository implements UserRepositoryInterface
                     })
                 )
                 ->when(
-                    is_null(request('has_admin_access')),
+                    !is_null(request('has_admin_access')),
                     fn(Builder $builder) => $builder->where(function($query){
-                        $query->where('role_id', '!=', User::ADMIN);
+                        $query->where('role_id', User::ADMIN);
                     })
                 )
                 ->when(
