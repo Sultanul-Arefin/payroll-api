4f33ea6263347f6d29c1115587ddecd02ebba181
akazad914@gmail.com <akazad914@gmail.com>
2024-06-11 15:52:50 +0600

    digital tax report generate done

---
+++ Modules/Dashboard/app/Transformers/DigitalTaxReportResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Modules\Dashboard\app\Transformers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class DigitalTaxReportResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'employee_name' => $this->employee?->name,
+            'tax_no' => $this->employee?->user_deatils?->fax,
+            'month' => $this->month,
+            'year' => date('Y', strtotime($this->first_date)),
+            'gross_pay_before_tax' => $this->gross_pay_before_tax,
+            'income_tax' => $this->tax_value,
+            'other_tax' => $this->tax_value,
+            'total_paid' => $this->net_pay,
+        ];
+    }
+}
