d5b41172d9a423012c2962347a35d60676fe2ac9
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-06-18 23:27:56 +0600

    comment

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,6 @@ Route::middleware(['json.response'])->prefix('v1')->group(function () {
             ->name('internal-report');
     });
 
-    //
+    // THIS
     Route::post('registration-from-bfin-technology', [RegisteredUserController::class, 'registration_from_bfin_technology']);
 });
