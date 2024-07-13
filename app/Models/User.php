316e9da63833d0beba2c8acbfc75d06f2ebfb4d4
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-07-13 15:29:15 +0600

    comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,8 +71,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
      */
     protected $hidden = [
         'password',
