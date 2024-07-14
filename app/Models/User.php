7cd5ed479dd417dff9f0a0b0d8bddb13808e4dae
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-07-14 15:29:31 +0600

    comments

--- app/Models/User.php
+++ app/Models/User.php
@@ -79,8 +79,6 @@ class User extends Authenticatable
 
     /**
      * The attributes that should be cast.
-     *
-     * @var array<string, string>
      */
     protected $casts = [
         'status' => 'integer',
