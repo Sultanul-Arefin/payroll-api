cb97ea6055df7de053be04103fa12a2e432946ae
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-08-13 21:55:24 +0600

    get timezone test

--- Modules/Attendance/Http/Controllers/AttendanceController.php
+++ Modules/Attendance/Http/Controllers/AttendanceController.php
@@ -50,13 +50,6 @@ class AttendanceController extends Controller
     public function store(Request $request)
     {
         date_default_timezone_set('Asia/Dhaka');
-        return apiResponse(
-            data: [
-                '1' => date_default_timezone_get(),
-                '2' => Carbon::now()->timezoneName,
-                '3' => config('app.timezone')
-            ]
-        );
         if (auth()->user()->user_details->attendance_type == UserDetails::MACHINE_ATTENDANCE) {
             return apiResponse(
                 data: [
