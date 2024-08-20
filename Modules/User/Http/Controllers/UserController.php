97ee96bb2d841101af7508faf0c3ee9064587771
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-08-20 17:45:01 +0600

    user show page updated

--- Modules/User/Http/Controllers/UserController.php
+++ Modules/User/Http/Controllers/UserController.php
@@ -68,7 +68,6 @@ class UserController extends Controller
     {
         $user->user_details = $user->user_details;
         $user->user_details['others'] = json_decode($user->user_details['others_number']);
-        return $user;
         $user->user_details['gender_value'] = $user->user_details?->gender == 0 ? 'Female' : 'Male';
         $user->user_details['user_image'] = $user->user_details?->user_image ? env('APP_URL').'/'.'storage/'.$user->user_details?->user_image : null;
         $user->department_name = $user->department?->department_name;
