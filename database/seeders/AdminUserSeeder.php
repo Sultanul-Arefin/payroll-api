c5772fe47a49154f82bf0ceba3329331f7036518
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-08-20 16:43:57 +0600

    updated admin user & white label seeder file

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Admin;
+use App\Models\WhiteLabelPartner;
 use Illuminate\Database\Seeder;
 
 class AdminUserSeeder extends Seeder
@@ -16,6 +17,19 @@ class AdminUserSeeder extends Seeder
             'email' => 'admin@bfin.it',
             'name' => 'Admin',
             'password' => bcrypt('password'), // password
+        ]);
+
+        $white_label = WhiteLabelPartner::create([
+            'name' => 'Admin',
+            'email' => 'admin@bfin.it',
+            'mobile' => 111,
+            'fax' => 111,
+            'country_id' => 1,
+            'address' => 'Test',
+            'contact_person_name' => 'Admin',
+            'contact_person_email' => 'admin@bfin.it',
+            'contact_person_number' => 111,
+            'password' => bcrypt('password')
         ]);
     }
 }
