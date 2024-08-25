22e350ac32566e7da2d1e875b4d6e823db4c20dc
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-08-25 11:57:21 +0600

    attendance response updated for admin

--- Modules/Attendance/Http/Resources/AttendanceResourceForUser.php
+++ Modules/Attendance/Http/Resources/AttendanceResourceForUser.php
@@ -37,8 +37,8 @@ class AttendanceResourceForUser extends JsonResource
             'working_hours_per_day' => $this->user?->company?->working_hours_per_day,
             'lunch_and_others_per_day' => $this->user?->company?->lunch_and_others_per_day,
             'total_office_hours' => $this->user?->company?->working_hours_per_day + $this->user?->company?->lunch_and_others_per_day,
-            'in_time' => $this->attendance_details[0]?->in_time,
-            'out_time' => $this->attendance_details[0]?->out_time
+            'in_time' => $this->attendance_details[0]?->in_time, // have to recheck this value
+            'out_time' => $this->attendance_details[0]?->out_time // have to recheck this value
         ];
     }
 
