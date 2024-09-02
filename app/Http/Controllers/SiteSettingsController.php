1fdc288cc0c1178419a1ad39b6178cc7efd4cb2e
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-09-02 15:48:52 +0600

    unsecured api

--- app/Http/Controllers/SiteSettingsController.php
+++ app/Http/Controllers/SiteSettingsController.php
@@ -7,11 +7,14 @@ use Illuminate\Http\Request;
 
 class SiteSettingsController extends Controller
 {
-    $site_settings = SiteSetting::first();
-    $site_settings->logo = $site_settings->changed_logo;
-    $site_settings->loader = $site_settings->changed_loader;
-    $site_settings->favicon = $site_settings->changed_favicon;
-    return $this->apiResponse(
-        data: $site_settings
-    );
+    public function index()
+    {
+        $site_settings = SiteSetting::first();
+        $site_settings->logo = $site_settings->changed_logo;
+        $site_settings->loader = $site_settings->changed_loader;
+        $site_settings->favicon = $site_settings->changed_favicon;
+        return $this->apiResponse(
+            data: $site_settings
+        );
+    }
 }
