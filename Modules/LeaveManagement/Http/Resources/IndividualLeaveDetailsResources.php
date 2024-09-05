18722036fedf8b1bf614d627d2fd3a8d7401cb2b
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-09-05 12:04:57 +0600

    resource class empty

--- Modules/LeaveManagement/Http/Resources/IndividualLeaveDetailsResources.php
+++ Modules/LeaveManagement/Http/Resources/IndividualLeaveDetailsResources.php
@@ -9,10 +9,7 @@ class IndividualLeaveDetailsResources extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'user_leaves_id' => $this->user_leaves_id,
-            'date' => $this->dates,
-            // 'details' => LeaveDetailsResourcel::collection($this->whenLoaded('leave_details'))
+
         ];
     }
 }
