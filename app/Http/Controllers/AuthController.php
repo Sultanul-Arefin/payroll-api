02c87805891551d6f1bd6c890cd2312375483819
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-09-15 11:41:03 +0600

    unnecessary lines

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -112,7 +112,6 @@ class AuthController extends Controller
         $status = Password::sendResetLink(
             $request->only('email')
         );
-
         return $status === Password::RESET_LINK_SENT
                     ? back()->with(['status' => __($status)])
                     : back()->withErrors(['email' => __($status)]);
