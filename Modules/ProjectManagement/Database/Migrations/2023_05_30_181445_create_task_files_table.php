ad022c5f99b986d2b4c6caed15e5765691a695fb
akazad1235 <akazad914@gmail.com>
2024-10-22 16:13:57 +0600

    update the task_files migration

--- Modules/ProjectManagement/Database/Migrations/2023_05_30_181445_create_task_files_table.php
+++ Modules/ProjectManagement/Database/Migrations/2023_05_30_181445_create_task_files_table.php
@@ -15,15 +15,16 @@ return new class extends Migration
     {
         Schema::create('task_files', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('task_id');
+            $table
+                ->foreignId('task_id')
+                ->constrained('tasks', 'id')
+                ->cascadeOnDelete();
             $table->string('files');
             $table
                 ->foreignId('uploaded_by')
                 ->constrained('users', 'id')
                 ->cascadeOnDelete();
             $table->timestamps();
-
-            $table->foreign('task_id')->on('id')->references('tasks')->onDelete('cascade');
         });
     }
 
