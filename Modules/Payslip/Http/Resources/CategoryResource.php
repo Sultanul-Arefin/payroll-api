4b05ab4ba24103bb430b9d4c9ce73fcf94cd32f0
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-12-15 13:42:40 +0600

    updating calculation for percentage

--- Modules/Payslip/Http/Resources/CategoryResource.php
+++ Modules/Payslip/Http/Resources/CategoryResource.php
@@ -226,6 +226,7 @@ class CategoryResource extends JsonResource
                 })
                 ->get()->sum('amount');
             $categoryOneAmount = $this->getCategoryIdOneAmount(1);
+            return $categoryOneAmount;
             $threshold = ($categoryOneAmount / $threshold) * 100;
             return round($straight + $threshold, 2);
         } else {
