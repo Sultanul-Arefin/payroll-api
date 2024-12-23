1012a96aaf05c5448581fbbe0ab3d5f34bdaecef
Sultanul-Arefin <fahimsultan4@gmail.com>
2024-12-23 08:22:36 +0600

    country wise salary items api's added

--- Modules/SalaryItemsName/Routes/api.php
+++ Modules/SalaryItemsName/Routes/api.php
@@ -19,5 +19,7 @@ Route::middleware(['json.response'])->prefix('v1')->group(function () {
             ->name('salary-items-name');
         Route::post('salary-items', [SalaryItemsNameController::class, 'store'])
             ->name('salary-items-name-store');
+        Route::post('country-wise-salary-item', [SalaryItemsNameController::class, 'country_wise_salary_item'])
+            ->name('country_wise_salary_item');
     });
 });
