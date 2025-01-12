e3c7198d1d49e0bde37def6d811e617aa20905c4
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-01-12 22:05:07 +0600

    integer check removed from frontend

--- Modules/EmployeeSalaryItems/Http/Controllers/EmployeeSalaryItemsController.php
+++ Modules/EmployeeSalaryItems/Http/Controllers/EmployeeSalaryItemsController.php
@@ -35,8 +35,8 @@ class EmployeeSalaryItemsController extends Controller
         if ($check_category->salary_items_category_id == 5) {
             $request->validate([
                 'tax_type' => 'required|in:straight,threshold',
-                'start_percentage_after' => 'required_if:tax_type,threshold|integer',
-                'end_percentage_at' => 'nullable|integer'
+                'start_percentage_after' => 'required_if:tax_type,threshold',
+                'end_percentage_at' => 'nullable'
             ]);
         }
         $employee_salary = DB::transaction(function () use ($request, $check_category) {
