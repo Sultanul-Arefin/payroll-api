f838a895d858f0fc902d1b33ae5d274bb603d81f
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-01-15 14:30:21 +0600

    wages updated

--- Modules/Payslip/Http/Services/PayslipService.php
+++ Modules/Payslip/Http/Services/PayslipService.php
@@ -201,7 +201,8 @@ class PayslipService
                     }
                 )
                 ->first('amount');
-            $hours_worked = $this->get_hours_worked(request('employee_id'), request('from_date'), request('to_date'));
+            $hours_worked = request('working_hours');
+            // $hours_worked = $this->get_hours_worked(request('employee_id'), request('from_date'), request('to_date'));
             if($hours_worked < 0){
                 $employee_associated_amount = 0;
             } else{
@@ -683,7 +684,8 @@ class PayslipService
                         }
                     )
                     ->first('amount');
-                $hours_worked = $this->get_hours_worked(request('employee_id'), request('from_date'), request('to_date'));
+                $hours_worked = request('working_hours');
+                // $hours_worked = $this->get_hours_worked(request('employee_id'), request('from_date'), request('to_date'));
                 if($hours_worked < 0){
                     $employee_associated_amount = 0;
                 } else{
