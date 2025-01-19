1044c21ff8cc4fe938d5d1c99e3ebb0ad244f3a7
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-01-19 12:03:26 +0600

    updated additinal pay

--- Modules/Payslip/Http/Controllers/PayslipController.php
+++ Modules/Payslip/Http/Controllers/PayslipController.php
@@ -378,7 +378,7 @@ class PayslipController extends Controller
         /**
          * $get_total_deduction_amount_for_attendance = $this->payslipService->get_total_deduction_amount_for_attendance($request->employee_id);
          */
-        $calculation = DB::transaction(function () use ($request, $get_basic, $get_staff_deduction_sick_absent, $total_pay, $get_taxable_allowance, $gross_pay_before_tax, $gross_pay_after_tax, $get_income_taxes, $get_additional_taxes_tax_top_up, $get_non_taxable_allowance, $pay_due_before_deductions, $total_amount, $hours_worked, $get_government_deduction, $get_other_complimentary_deduction, $get_company_contribution_value, $get_employee_contribution_value, $get_other_company_deduction, $get_other_company_contribution) {
+        $calculation = DB::transaction(function () use ($request, $get_basic, $get_category_one_other_values, $get_staff_deduction_sick_absent, $total_pay, $get_taxable_allowance, $gross_pay_before_tax, $gross_pay_after_tax, $get_income_taxes, $get_additional_taxes_tax_top_up, $get_non_taxable_allowance, $pay_due_before_deductions, $total_amount, $hours_worked, $get_government_deduction, $get_other_complimentary_deduction, $get_company_contribution_value, $get_employee_contribution_value, $get_other_company_deduction, $get_other_company_contribution) {
             /** create payslip */
             $payslip = Payslip::create([
                 'employee_id' => $request->employee_id,
@@ -390,6 +390,7 @@ class PayslipController extends Controller
                 'payment_date' => $request->payment_date,
                 'hours_worked' => $hours_worked,
                 'wages' => $get_basic,
+                'additional_pay' => $get_category_one_other_values,
                 'leave_deduction' => $get_staff_deduction_sick_absent,
                 'total_pay_value' => $total_pay,
                 'taxable_allowance' => $get_taxable_allowance,
