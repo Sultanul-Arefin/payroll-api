1f50247e218a1968929afcc4e209079d3dfa26f1
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-01-20 18:21:48 +0600

    updated payslip calculation

--- Modules/Payslip/Http/Resources/ViewPayslipResource.php
+++ Modules/Payslip/Http/Resources/ViewPayslipResource.php
@@ -222,10 +222,14 @@ class ViewPayslipResource extends JsonResource
             $payslip_detail->base_amount_or_hours = $payslip_detail->base_amount_or_hours;
             $payslip_detail->rate = $payslip_detail->rate;
             $payslip_detail->amount = $payslip_detail->amount;
-            $payslip_detail->category_id = $payslip_detail?->salary_item?->salaryItemsCategory?->id;
+            if (in_array($payslip_detail->salary_item->name, ["Bonus", "Overtime Rate", "Double Overtime Rate"])) {
+                $payslip_detail->category_id = $payslip_detail?->salary_item?->salaryItemsCategory?->id . "_additional";
+            } else {
+                $payslip_detail->category_id = $payslip_detail?->salary_item?->salaryItemsCategory?->id;
+            }
 
             // unset these keys from the response
-            unset($payslip_detail->salary_item, $payslip_detail->id, $payslip_detail->amount, $payslip_detail->created_at, $payslip_detail->updated_at, $payslip_detail->payslip_id, $payslip_detail->salary_item_id);
+            unset($payslip_detail->salary_item, $payslip_detail->id, $payslip_detail->created_at, $payslip_detail->updated_at, $payslip_detail->payslip_id, $payslip_detail->salary_item_id);
         }
         return $payslip_details;
     }
