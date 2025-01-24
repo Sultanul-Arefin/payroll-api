06465349bd231240df80bea4ec1a4f489d7c2729
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-01-24 11:51:39 +0600

    fillable property updated

--- Modules/Payslip/Entities/PayslipDetail.php
+++ Modules/Payslip/Entities/PayslipDetail.php
@@ -5,6 +5,7 @@ namespace Modules\Payslip\Entities;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Modules\EmployeeSalaryItems\Entities\EmployeeSalaryItem;
 use Modules\SalaryItemsName\Entities\SalaryItemsName;
 
 class PayslipDetail extends Model
@@ -14,6 +15,7 @@ class PayslipDetail extends Model
     protected $fillable = [
         'payslip_id',
         'salary_item_id',
+        'employee_salary_item_id',
         'amount',
         'base_amount_or_hours',
         'rate'
@@ -25,5 +27,21 @@ class PayslipDetail extends Model
     public function salary_item(): BelongsTo
     {
         return $this->belongsTo(SalaryItemsName::class, 'salary_item_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function employee_salary_item(): BelongsTo
+    {
+        return $this->belongsTo(EmployeeSalaryItem::class, 'employee_salary_item_id', 'id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function payslip(): BelongsTo
+    {
+        return $this->belongsTo(Payslip::class, 'payslip_id', 'id');
     }
 }
