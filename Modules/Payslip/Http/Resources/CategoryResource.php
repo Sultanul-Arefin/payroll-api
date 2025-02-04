49ddfc09f1649db37ff5426137b516742457a286
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-02-04 17:47:37 +0600

    threshold calculation updated

--- Modules/Payslip/Http/Resources/CategoryResource.php
+++ Modules/Payslip/Http/Resources/CategoryResource.php
@@ -346,7 +346,7 @@ class CategoryResource extends JsonResource
                     $start_percentage_after = $threshold_details->start_percentage_after;
                     $end_percentage_at = $threshold_details->end_percentage_at;
 
-                    if ($start_percentage_after <= $gross_pay_before_tax && $end_percentage_at >= $gross_pay_before_tax) {
+                    if ($start_percentage_after <= $gross_pay_before_tax && (is_null($end_percentage_at) || $end_percentage_at >= $gross_pay_before_tax)) {
                         $threshold_value += ($gross_pay_before_tax * ($percentage_amount / 100));
                     }
                 }
