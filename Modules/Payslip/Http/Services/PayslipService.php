e774804599f5b31dcce977f01ce0a2999d26853d
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-02-06 13:17:20 +0600

    updated threshold

--- Modules/Payslip/Http/Services/PayslipService.php
+++ Modules/Payslip/Http/Services/PayslipService.php
@@ -320,7 +320,7 @@ class PayslipService
                 $end = $threshold_details->end_percentage_at;
 
                 if($gross_pay_before_tax > $start){
-                    $taxableAmount = ($end === null || $gross_pay_before_tax < $end) ? $gross_pay_before_tax - $start : $end - $start;
+                    $taxableAmount = ($end === null || $gross_pay_before_tax < $end) ? ($gross_pay_before_tax - $start) + 1 : ($end - $start) + 1;
 
                     return $taxableAmount;
                     // $threshold_value += $taxableAmount * ($percentage_amount / 100);
