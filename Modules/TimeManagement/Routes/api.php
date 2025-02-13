f55b26b59aa35b273051534ad792a14221eb874e
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-02-13 12:53:45 +0600

    bonus remove api added

--- Modules/TimeManagement/Routes/api.php
+++ Modules/TimeManagement/Routes/api.php
@@ -8,6 +8,7 @@ Route::middleware(['json.response'])->prefix('v1')->group(function () {
     Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('time-management-report', [TimeManagementController::class, 'time_management_report']);
         Route::get('attendance-report', [TimeManagementController::class, 'attendance_report']);
+        Route::post('delete-bonus/{bonus}', [TimeManagementController::class, 'delete_bonus']);
         Route::post('add-overtime-doubleOvertime-bonus', []);
         Route::get('attendance-calendar-overview-per-person', [TimeManagementController::class, 'attendance_calendar_overview_per_person']);
     });
