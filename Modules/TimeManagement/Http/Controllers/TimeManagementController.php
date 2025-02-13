8599c4fccbfb90f26fec637659e7a1ad17c0fc2a
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-02-13 12:53:51 +0600

    bonus removed

--- Modules/TimeManagement/Http/Controllers/TimeManagementController.php
+++ Modules/TimeManagement/Http/Controllers/TimeManagementController.php
@@ -2,8 +2,10 @@
 
 namespace Modules\TimeManagement\Http\Controllers;
 
+use App\Models\Bonus;
 use App\Models\User;
 use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Attendance\Entities\Attendance;
@@ -39,11 +41,59 @@ class TimeManagementController extends Controller
                 ->where('status', User::USER_ACTIVE)
                 ->where('role_id', '!=', User::ADMIN)
                 ->get();
-        $response = AttendanceReportResource::collection(
+        return AttendanceReportResource::collection(
             $users
-        );
+        )->additional([
+            'meta' => [
+                'bonus_report' => $this->get_bonus_report($request->from_date, $request->to_date)
+            ],
+        ]);
+    }
+
+    public function get_bonus_report($from_date, $to_date){
+        $bonus = Bonus::query()
+                ->whereHas(
+                    'user', function (Builder $builder) {
+                        $builder
+                            ->where('company_id', auth()->user()->company_id);
+                    }
+                )
+                ->whereBetween(
+                    'date',
+                    [
+                        $from_date,
+                        $to_date
+                    ]
+                )
+                ->get();
+        return $bonus->map(function($q){
+            $q->employee_name = $q?->user?->name;
+            $q->department_name = $q->user->department?->department_name;
+            $q->bonus_type = match($q->type){
+                Bonus::BONUS_HOURLY => "Hourly",
+                Bonus::BONUS_SALARY_BASIC => "Salary Basic",
+                Bonus::BONUS_DIRECT_AMOUNT => "Direct Amount",
+                Bonus::OVERTIME => "Overtime",
+                Bonus::DOUBLE_OVERTIME => "Double Overtime",
+                Bonus::RECUPERATED => "Recuperated",
+            };
+            $q->given_amount = $q->given_amount;
+            $q->generalize_amount = $q->generalize_amount;
+            $q->given_date = $q->date;
+            $q->month = date('F', strtotime($q->date));
+
+            unset($q->type, $q->created_at, $q->updated_at, $q->date, $q->user);
+
+            return $q;
+        });
+    }
+
+    public function delete_bonus(Bonus $bonus){
+        $bonus->delete();
+
         return apiResponse(
-            data: $response
+            data: null,
+            message: 'Bonus Successfully Deleted'
         );
     }
 
