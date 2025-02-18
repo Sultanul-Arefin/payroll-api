580812c3996b649f6b1a6d8ce8aa76f956ef8a78
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-02-18 13:09:27 +0600

    digital tax report controller updated for pdf

--- Modules/Dashboard/Http/Controllers/ReportController.php
+++ Modules/Dashboard/Http/Controllers/ReportController.php
@@ -144,8 +144,21 @@ class ReportController extends Controller
             ->whereYear('first_date', $request->year)
             ->with('employee', 'employee.user_details')
             ->get();
+
+        $total['total_wages'] = 0;
+        $total['total_taxable_value'] = 0;
+        $total['total_tax_deducted'] = 0;
+        $total['total_net_pay'] = 0;
+
+        foreach($reports as $value){
+            $total['total_wages'] += $value->wages;
+            $total['total_taxable_value'] += $value->taxable_allowance;
+            $total['total_tax_deducted'] += $value->tax_value;
+            $total['total_net_pay'] += $value->net_pay;
+        }
         $data = [
-            'data' => $reports
+            'data' => $reports,
+            'total' => $total
         ];
         $pdf = PDF::loadView('reports.digital_tax_report', $data);
         $options = $pdf->getOptions();
