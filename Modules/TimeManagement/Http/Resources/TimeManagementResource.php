30365bd064084385226292a8bdba361827e4ddac
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-02-24 12:49:34 +0600

    leave-count API updated to the current year calculation

--- Modules/TimeManagement/Http/Resources/TimeManagementResource.php
+++ Modules/TimeManagement/Http/Resources/TimeManagementResource.php
@@ -2,6 +2,7 @@
 
 namespace Modules\TimeManagement\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Modules\LeaveManagement\Entities\UserLeave;
@@ -18,11 +19,11 @@ class TimeManagementResource extends JsonResource
             'employee_id' => 'EMP-ID',
             'department' => $this?->department?->department_name,
             'annual_leave_quota' => $this->getAnnualLeaveQuota($this->company),
-            'annual_leave_taken' => $this->getAnnualLeaveTaken($this->id),
-            'remaining_annual_leave' => $this->getAnnualLeaveQuota($this->company) - $this->getAnnualLeaveTaken($this->id),
+            'annual_leave_taken' => $this->getAnnualLeaveTaken($this->id, Carbon::now()->year),
+            'remaining_annual_leave' => $this->getAnnualLeaveQuota($this->company) - $this->getAnnualLeaveTaken($this->id, Carbon::now()->year),
             'sick_leave_quota' => $this->getSickLeaveQuota($this->company),
-            'sick_leave_taken' => $this->getSickLeaveTaken($this->id),
-            'remaining_sick_leave' => $this->getSickLeaveQuota($this->company) - $this->getSickLeaveTaken($this->id),
+            'sick_leave_taken' => $this->getSickLeaveTaken($this->id, Carbon::now()->year),
+            'remaining_sick_leave' => $this->getSickLeaveQuota($this->company) - $this->getSickLeaveTaken($this->id, Carbon::now()->year),
             // 'unpaid_sick_leave(absent)' => 0,
             'unpaid_sick_leave_absent' => 0,
             'maternity_leave' => 0
@@ -47,7 +48,7 @@ class TimeManagementResource extends JsonResource
         return $data->no_of_days;
     }
 
-    function getAnnualLeaveTaken($user_id): int {
+    function getAnnualLeaveTaken($user_id, $current_year): int {
         $annual_leave_data = LeaveSalaryItems::query()
             ->whereHas(
                 'salary_items_name', function(Builder $builder){
@@ -66,6 +67,9 @@ class TimeManagementResource extends JsonResource
                 ->where('user_id', $user_id)
                 ->where('leave_type', $annual_leave_data->salary_items_id)
                 ->where('status', UserLeave::APPROVED)
+                ->whereHas('leave_details', function(Builder $builder) use($current_year){
+                    $builder->whereYear('dates', $current_year);
+                })
                 ->get();
         $count = 0;
         foreach($data as $value){
@@ -95,7 +99,7 @@ class TimeManagementResource extends JsonResource
         return $data->no_of_days;
     }
 
-    function getSickLeaveTaken($user_id): int {
+    function getSickLeaveTaken($user_id, $current_year): int {
         $sick_leave_data = LeaveSalaryItems::query()
             ->whereHas(
                 'salary_items_name', function(Builder $builder){
@@ -114,6 +118,9 @@ class TimeManagementResource extends JsonResource
                 ->where('user_id', $user_id)
                 ->where('leave_type', $sick_leave_data->salary_items_id)
                 ->where('status', UserLeave::APPROVED)
+                ->whereHas('leave_details', function(Builder $builder) use($current_year){
+                    $builder->whereYear('dates', $current_year);
+                })
                 ->get();
         $count = 0;
         foreach($data as $value){
