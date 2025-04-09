42ae723bf231304ab05e4e121024d78c3e11ed5b
shoriful-islam-shorif <shoriful.islam5435@gmail.com>
2025-04-09 12:35:12 +0600

    south african- payslip- update

--- Modules/User/Database/Migrations/2023_05_15_113923_UserDetails.php
+++ Modules/User/Database/Migrations/2023_05_15_113923_UserDetails.php
@@ -54,6 +54,7 @@ return new class extends Migration
             $table->string('religion')->nullable();
             $table->string('health_insurance')->nullable();
             $table->string('pension_administrator')->nullable();
+            $table->string('pension_pin')->nullable();
             $table->timestamps();
             
         });
