9c91e20b7af7af7de4fb2a6e75a4059b1eda1af9
Sultanul-Arefin <fahimsultan4@gmail.com>
2025-04-22 15:12:22 +0600

    requested attendance for admin response updated

--- Modules/Attendance/Http/Resources/AttendanceDetailsResourceForAdmin.php
+++ Modules/Attendance/Http/Resources/AttendanceDetailsResourceForAdmin.php
@@ -3,12 +3,14 @@
 namespace Modules\Attendance\Http\Resources;
 
 use App\Models\Overtime;
+use App\Models\User;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use JsonSerializable;
 use Modules\Attendance\Entities\Attendance;
 use Modules\Attendance\Entities\AttendanceDetail;
+use Modules\User\Entities\UserDetails;
 
 class AttendanceDetailsResourceForAdmin extends JsonResource
 {
@@ -29,9 +31,24 @@ class AttendanceDetailsResourceForAdmin extends JsonResource
             'is_deletable' => $this->getStatus($this->status) == "pending" ? 1 : 0,
             'is_editable' => $this->getStatus($this->status) == "pending" ? 1 : 0,
             'is_overtime' => $this->overtime ? 1 : 0,
-            'overtime_value' => $this->getOvertime($this->overtime)
+            'overtime_value' => $this->getOvertime($this->overtime),
+            'attendance_type_key' => $this->user->user_details?->attendance_type,
+            'attendance_type_value' => $this->user->user_details?->attendance_type == UserDetails::WEB_ATTENDANCE ? 'web' : 'machine',
+            'total_office_hours' => $this->user->company?->working_hours_per_day + $this->user->company?->lunch_and_others_per_day,
+            'employee_type' => $this->get_employee_type_for_manual_attendance($this->user)
             
         ];
+    }
+
+    public function get_employee_type_for_manual_attendance($user)
+    {
+        $type = $user->employee_type;
+        return match($type){
+            User::EMPLOYEE_TYPE_FULL_TIME => "full_time",
+            User::EMPLOYEE_TYPE_PART_TIME => "part_time",
+            User::EMPLOYEE_TYPE_FLEXI_TIME => "flexi_time",
+            User::EMPLOYEE_TYPE_CONTRACTUAL => "contractual"
+        };
     }
 
     public function getOvertime($overtime){
